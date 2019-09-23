6cb9c088fcd06a0b1d9b72a27a5757e40eb618b1
Patricio Vargas <pevargas2730@gmail.com>
2019-09-23 17:11:51 -0400

    src api created

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,13 @@
 /*
   Express route controllers for all the endpoints of the app
 */
+
+const Router = require('express');
+
+// guaranteed to get dependencies
+const apiRoutes = () => {
+  const app = Router();
+  return app;
+};
+
+module.exports = apiRoutes;
