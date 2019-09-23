f1df05533d34dfc915196e50c778fbeed18461ef
Patricio Vargas <pevargas2730@gmail.com>
2019-09-23 18:43:29 -0400

    code cleanup

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,23 +1,16 @@
 /*
     App Entry Point
 */
-
-//const logger = require('./loaders/logger');
 const express = require('express');
 const body_parser = require('body-parser');
 const cors = require('cors');
 const config = require('../config');
-const apiRoutes = require('../api/index');
+const apiRoutes = require('../api');
 const app = express();
 
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
 app.use(cors());
-
-app.get('/', function(req, res, next) {
-  res.json({ msg: 'This is CORS-enabled for all origins!' });
-});
-
 app.use(config.api.prefix, apiRoutes());
 
 /// catch 404 and forward to error handler
