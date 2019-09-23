b47ee2c1ecbd931b9a1cca1105949386c8b311f9
Patricio Vargas <pevargas2730@gmail.com>
2019-09-23 15:33:58 -0400

    npm i nodemon

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
 /*
     App Entry Point
 */
+import config from './config';
+
+import express from 'express';
+
+import Logger from './loaders/logger';
+const express = require('express');
+const app = express();
