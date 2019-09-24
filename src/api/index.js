f32563c0a22b83e4c724bb4101854cbd80d4419d
Patricio Vargas <pevargas2730@gmail.com>
2019-09-24 01:00:14 -0400

    added auth routes

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,14 @@
 */
 
 const todo = require('../api/routes/todo');
+const auth = require('../api/routes/auth');
 const express = require('express');
 
 // guaranteed to get dependencies
 const apiRoutes = () => {
   const router = express.Router();
   todo(router);
+  auth(router);
   return router;
 };
 
