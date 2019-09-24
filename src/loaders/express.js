cee1ddd0b8d70fa121266a366e0ebeadcf70d167
Patricio Vargas <pevargas2730@gmail.com>
2019-09-24 00:59:04 -0400

    express js clean up

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,12 +1,17 @@
 /*
     App Entry Point
 */
-const express = require('express');
-const body_parser = require('body-parser');
-const cors = require('cors');
 const config = require('../config');
 const apiRoutes = require('../api');
+const mongooseConnection = require('../loaders/mongoose');
+const express = require('express');
+const body_parser = require('body-parser');
+const Report = require('../models/report');
+const cors = require('cors');
 const app = express();
+
+mongooseConnection();
+console.log(`  ✌️ DB loaded and connected!`);
 
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
