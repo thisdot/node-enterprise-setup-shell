ef53e602fda418bba94cb46d61197900a0f639c8
Patricio Vargas <pevargas2730@gmail.com>
2019-09-24 00:59:32 -0400

    added todo routes

--- src/api/routes/todo.js
+++ src/api/routes/todo.js
@@ -1,7 +1,25 @@
-const express = require('express');
+const TodoService = require('../../services/todo');
+const isAuth = require('../middleware/checkAuth');
 const todo = router => {
-  router.get('/', (req, res, next) => {
-    return res.json({ msg: 'This is CORS-enabled for all origins!' }).status(200);
+  /* Get All Tasks */
+  router.get('/tasks', isAuth, async (req, res, next) => {
+    try {
+      const results = await TodoService.getTasks();
+      return res.status(201).json(results);
+    } catch (e) {
+      return next(e);
+    }
+  });
+
+  /* Get Task by ID */
+  router.get('/tasks/:id', async (req, res, next) => {
+    try {
+      const taskId = req.params.id;
+      const task = await TodoService.getTasksById(taskId);
+      return res.status(200).json(task);
+    } catch (e) {
+      return next(e);
+    }
   });
 };
 
