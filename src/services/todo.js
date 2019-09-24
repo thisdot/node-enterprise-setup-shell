5bb76915bd8a1511dca1d8f79caeda38b2141a1d
Patricio Vargas <pevargas2730@gmail.com>
2019-09-24 00:55:24 -0400

    created todo services

--- src/services/todo.js
+++ src/services/todo.js
@@ -1,3 +1,47 @@
 /*
     All the business logic is here
 */
+
+const TaskModel = require('../models/task');
+
+class TodoService {
+  static async getTasks() {
+    try {
+      const tasks = await TaskModel.find();
+      return {
+        message: 'Fetched posts successfully.',
+        tasks: tasks,
+        status: 200
+      };
+    } catch (err) {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      return err;
+    }
+  }
+
+  static async getTasksById(id) {
+    const taskId = id;
+    const task = await TaskModel.findById(taskId);
+    try {
+      if (!task) {
+        const ERROR = new Error('Task not found');
+        ERROR.statusCode = 404;
+        throw ERROR;
+      }
+      return {
+        message: 'Task feched',
+        task: task,
+        status: 200
+      };
+    } catch (err) {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      return err;
+    }
+  }
+}
+
+module.exports = TodoService;
