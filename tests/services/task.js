407c4747e6b7618c7cc9160b303487874eeaa381
Patricio Vargas <pevargas2730@gmail.com>
2019-09-25 01:10:49 -0400

    wrote tests

--- tests/services/task.js
+++ tests/services/task.js
@@ -1,12 +1,44 @@
 /* Sample Test */
-'use strict';
-const chai = require('chai');
-const sinon = require('sinon');
-const expect = chai.expect;
-chai.should();
 
-describe('number tests', () => {
-  describe('isEven', () => {
-    it('should return true when a number is even', () => {});
+'use strict';
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.should();
+chai.use(chaiAsPromised);
+
+const TodoService = require('../../src/services/todo');
+const mongoDB = require('../../src/loaders/mongoose');
+
+describe('Todo Service', () => {
+  const task = { _id: '5d89973cf47adf55979b38c0', title: 'test', content: 'test', __v: 0 };
+
+  before(() => mongoDB.connect());
+
+  it('tasks length should be at least one', async () => {
+    const results = await TodoService.getTasks();
+    results.tasks.length.should.be.least(1);
+    if (results.task === 0) {
+      throw new Error('No data!');
+    }
   });
+
+  it('should resolve', () => {
+    return TodoService.getTasks();
+  });
+
+  it('should get proper task by ID', async () => {
+    const result = await TodoService.getTasksById(task._id);
+    const id = result.task._doc._id.toString();
+    id.should.equal(task._id);
+    Object.keys(result.task._doc).length.should.equal(4);
+  });
+
+  it('full fills the promise with the correct task', async () => {
+    const result = await TodoService.getTasksById(task._id);
+    result.task._doc._id = result.task._id.toString();
+    result.task._doc.should.eql(task);
+  });
+
+  after(() => mongoDB.disconnect());
 });
