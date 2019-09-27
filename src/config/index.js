5a1b1bbf547b9ce1c00ea335144f4516983223e2
Patricio Vargas <pevargas2730@gmail.com>
2019-09-27 03:17:45 -0400

    code cleanup

--- src/config/index.js
+++ src/config/index.js
@@ -5,42 +5,20 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFound = dotenv.config();
 if (!envFound) {
-  // This error should crash whole process
-
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
 const config = {
   dev: {
-    /**
-     * Your favorite port
-     */
     port: parseInt(process.env.PORT, 10),
-
-    /**
-     * That long string from Mongo ATLAS
-     */
     database_URL: process.env.MONGODB_URI_DEV,
-    /**
-     * API configs
-     */
     api: {
       prefix: '/api'
     }
   },
   prod: {
-    /**
-     * Your favorite port
-     */
     port: parseInt(process.env.PORT, 10),
-
-    /**
-     * That long string from Mongo ATLAS
-     */
     database_URL: process.env.MONGODB_URI_PROD,
-    /**
-     * API configs
-     */
     api: {
       prefix: '/api'
     }
