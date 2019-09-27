27888b4fe0d54f108a8c0b85b45d67e3c2121c50
Patricio Vargas <pevargas2730@gmail.com>
2019-09-27 10:11:40 -0400

    index api cleanup

--- src/api/index.js
+++ src/api/index.js
@@ -2,16 +2,13 @@
   Express route controllers for all the endpoints of the app
 */
 
-const todo = require('../todo/routes/todo');
 const isAuth = require('./middleware/checkAuth');
 const express = require('express');
 
-// guaranteed to get dependencies
 const apiRoutes = () => {
   const router = express.Router();
   //Router level middleware router.use
-  router.use(isAuth);
-  router.use(todo);
+
   return router;
 };
 
