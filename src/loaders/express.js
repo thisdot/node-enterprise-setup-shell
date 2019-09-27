c331e394d388b4cfaa0e153c3955f44d0a1484ce
Patricio Vargas <pevargas2730@gmail.com>
2019-09-27 10:16:44 -0400

    clean up

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -17,7 +17,7 @@ console.log(`  ✌️ DB loaded and connected!`);
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
 app.use(cors());
-app.use(config().api.prefix, apiRoutes()); //api
+app.use(config().api.prefix, apiRoutes());
 
 /// catch 404 and forward to error handler
 app.use((req, res, next) => {
