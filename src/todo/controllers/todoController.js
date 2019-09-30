3b9455950920e0bc24b6cd78b27e815a5e6e45e4
Patricio Vargas <pevargas2730@gmail.com>
2019-09-30 11:29:34 -0400

    code cleanup

--- src/todo/controllers/todoController.js
+++ src/todo/controllers/todoController.js
@@ -1,23 +1,10 @@
 /* Todo Controller */
 
-const todoService = require('../services/todo');
-const Task = require('../models/task');
-
 module.exports = {
   //EXAMPLE CONTROLLER
-  getTask: async (req, res, next) => {
+  YOUR_FUNCTION_NAME: async (req, res, next) => {
     try {
-      const tasks = await todoService.getTasks();
-      return res.status(200).json({
-        message: 'Fetched all data',
-        tasks: tasks,
-        status: 200
-      });
-    } catch (err) {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      return err;
-    }
+      return;
+    } catch {}
   }
 };
